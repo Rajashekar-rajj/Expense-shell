fe5b69282ecc01bc2725f90595de3689338b5534
rajashekar-rajj <rajashekarmekala2468@gmail.com>
2024-12-26 20:12:09 +0530

    mysql

--- backend.sh
+++ backend.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP= $(date+%Y-%m-%d-%H-%M-%s)
-LOG_FILE= "$LOG_FILE/$SCRIPT_NAME-$TIMESTAMP.log"
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
  
 USERID=$(id -u)
